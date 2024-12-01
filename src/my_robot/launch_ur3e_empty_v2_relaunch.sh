f0139da350056eca70fb2682ad903b541956722b
sounderya venugopal <vvsounderya22@gmail.com>
2024-12-02 01:51:37 +0530

    Task: Added robot model in gazebo with moveit config

---
+++ src/my_robot/launch_ur3e_empty_v2_relaunch.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# We remove a folder that otherwise gives issues in ROS2 launches
+sudo rm -r /home/user/.ros
+
+# We set up the environment for ROS2
+. /usr/share/gazebo/setup.sh
+export arm_ws=$(pwd)
+export GAZEBO_RESOURCE_PATH=$(arm_ws)/ur_descriptioin:${GAZEBO_RESOURCE_PATH}
+export GAZEBO_MODEL_PATH=$(arm_ws)/robotnik_sensors:${GAZEBO_MODEL_PATH}
+
+ros2 launch office_gazebo empty_ur3e.launch.xml
+
+
