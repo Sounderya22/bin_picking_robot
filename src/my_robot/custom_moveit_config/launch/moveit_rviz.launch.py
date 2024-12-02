e6567e2abe896ddddbd780d761ebfe22ff720b52
sounderya venugopal <vvsounderya22@gmail.com>
2024-12-03 03:59:59 +0530

    changes for naming convention

--- src/my_robot/custom_moveit_config/launch/moveit_rviz.launch.py
+++ src/my_robot/custom_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("name", package_name="test_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("name", package_name="custom_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
